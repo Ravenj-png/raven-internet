e90f4af66d0172cc0df7d1afb0d8d8883dbaca36
Ravenj-png <nyombijo57@gmail.com>
2026-05-29 23:03:23 +0300

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,24 @@
-javascript
 const CACHE_NAME = 'raven-pwa-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS).catch(err => {
+        console.log('Cache add failed:', err);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
