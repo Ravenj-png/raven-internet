d0d92e46aee8fec25adc2f35ce6ef9a8fc1fa205
Ravenj-png <nyombijo57@gmail.com>
2026-07-01 23:04:01 +0000

    Create rune_tokens.py

---
+++ backend/utils/rune_tokens.py
@@ -0,0 +1,13 @@
+RUNE_KEYWORDS = {
+    "is": "IS", "be": "BE",
+    "greater than": "GREATER_THAN", "less than": "LESS_THAN",
+    "at least": "AT_LEAST", "at most": "AT_MOST",
+    "equal to": "EQUAL_TO", "not equal to": "NOT_EQUAL_TO",
+    "and": "AND", "or": "OR", "not": "NOT",
+    "otherwise": "OTHERWISE", "end": "END",
+    "repeat": "REPEAT", "times": "TIMES",
+    "each": "EACH", "in": "IN", "while": "WHILE",
+    "function": "FUNCTION", "property": "PROPERTY", "class": "CLASS",
+    "into": "INTO", "stop": "STOP", "return": "RETURN",
+    "let": "LET", "add": "ADD", "remove": "REMOVE", "take": "TAKE"
+}
