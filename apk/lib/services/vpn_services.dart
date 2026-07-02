4fec2c5b280fc2d0ab8fa2b895ab75b37a671ab3
Ravenj-png <nyombijo57@gmail.com>
2026-07-02 22:51:13 +0000

    Update vpn_services.dart

--- apk/lib/services/vpn_services.dart
+++ apk/lib/services/vpn_services.dart
@@ -1,4 +1,18 @@
 import 'package:wireguard_flutter/wireguard_flutter.dart';
-class VpnService { static final wg=WireGuardFlutter.instance;
-  static Future<void> connect(String cfg, String ep) async { await wg.initialize(interfaceName:'wg0'); await wg.startVpn(serverAddress:ep, wgQuickConfig:cfg, providerBundleIdentifier:'com.ravenvpn.app'); }
-  static Future<void> disconnect() => wg.stopVpn(); }
+
+class VpnService {
+  static final wg = WireGuardFlutter.instance;
+
+  static Future<void> connect(String cfg, String ep) async {
+    await wg.initialize(interfaceName: 'wg0');
+    await wg.startVpn(
+      serverAddress: ep,
+      wgQuickConfig: cfg,
+      providerBundleIdentifier: 'com.ravenvpn.app',
+    );
+  }
+
+  static Future<void> disconnect() async {
+    await wg.stopVpn();
+  }
+}
